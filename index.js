e62d6aa3a5dbdc40c2504d09852b08aa83fa23ac
SadiaAfridi663 <sadiaafridi663@gmail.com>
2024-07-06 22:38:42 +0500

    problem 2

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-var userinput = +prompt("please enter a number")
-for(var i = 1; i <= 10; i++){
-    console.log(`${userinput} * ${i} = ${userinput * i}`)
-}
+// var userinput = +prompt("please enter a number")
+// for(var i = 1; i <= 10; i++){
+//     console.log(`${userinput} * ${i} = ${userinput * i}`)
+// }
+
+
+// problem 2
+// var numbers = []
+// for (var i = 1; i <= 5; i++){
+//     var num = +prompt("please enter a number");
+//     numbers.push(num)
+    
+    
+// }
+// var sum = 0;
+// for(var i = 0; i < 5; i++){ 
+//     sum += numbers[i];
+// }
+// console.log(sum);
+
+
+
