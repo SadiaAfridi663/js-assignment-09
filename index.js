4d3b64bc37bb418b998084543a96c5d7184d47c6
SadiaAfridi663 <sadiaafridi663@gmail.com>
2024-07-07 00:00:54 +0500

    problem 4, 5

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// problem 1
 // var userinput = +prompt("please enter a number")
 // for(var i = 1; i <= 10; i++){
 //     console.log(`${userinput} * ${i} = ${userinput * i}`)
@@ -8,15 +9,31 @@
 // var numbers = []
 // for (var i = 1; i <= 5; i++){
 //     var num = +prompt("please enter a number");
-//     numbers.push(num)
-    
-    
+//     numbers.push(num)   
 // }
 // var sum = 0;
 // for(var i = 0; i < 5; i++){ 
 //     sum += numbers[i];
 // }
 // console.log(sum);
+
+
+// //  problem 5
+// var num = +prompt("please enter a number");
+// var factorial = 1;
+// for(var i = num; i > 0; i--){
+//     factorial *= i;
+// }
+// console.log(`factorial of ${num} = ${factorial}`)
+
+
+// problem 4
+// var userinput = prompt("please enter any string");
+// var reversedString = "";
+// for (var i = userinput.length - 1 ; i >= 0; i--){
+//     reversedString += userinput[i];
+// }
+// console.log(reversedString);
 
 
 
